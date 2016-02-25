a5380d87e351239a5c6ebfb09cbb6cb911b76990
M.S.Kinting <M.S.Kinting>
2016-02-25 19:53:16 +0100

    added a Login Screen

---
+++ src/main/java/de/fa/controller/LoginHandler.java
@@ -0,0 +1,15 @@
+package de.fa.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class LoginHandler {
+	
+	public void login(){
+		
+		
+	}
+
+}
