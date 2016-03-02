d3513882d9fe992e786501a4c39bdae92cf91efa
M.S.Kinting <M.S.Kinting>
2016-03-02 18:50:51 +0100

    many fixes

--- src/main/java/de/fa/model/SearchResult.java
+++ src/main/java/de/fa/model/SearchResult.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 public class SearchResult implements Serializable{
 	
 	private static final long serialVersionUID = 762560152864528174L;
-	private ClockTime depature;
-	private ClockTime arrival;
+	private String depature;
+	private String arrival;
 	private String station;
 	private String line;
 	
@@ -14,30 +14,35 @@ public class SearchResult implements Serializable{
 	public SearchResult(){
 		
 	}
-	
+	/*
 	public SearchResult(ClockTime depature, ClockTime arrival, String station, String line){
 		this.depature = depature;
 		this.arrival = arrival;
 		this.station = station;
 		this.line = line;
 	}
+	*/
+	public SearchResult(String depature, String arrival, String station, String line){
+		this.depature = depature;
+		this.arrival = arrival;
+		this.station = station;
+		this.line = line;
+	}
 
-	public ClockTime getDepature() {
+	
+	
+	public String getDepature() {
 		return depature;
 	}
-
-	public void setDepature(ClockTime depature) {
+	public void setDepature(String depature) {
 		this.depature = depature;
 	}
-
-	public ClockTime getArrival() {
+	public String getArrival() {
 		return arrival;
 	}
-
-	public void setArrival(ClockTime arrival) {
+	public void setArrival(String arrival) {
 		this.arrival = arrival;
 	}
-
 	public String getStation() {
 		return station;
 	}
